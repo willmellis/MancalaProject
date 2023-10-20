02b58f5f56b602ca36828d81ef5c8fec34cfeba8
karlsyriani <syriani.karl@gmail.com>
2023-10-19 22:28:28 -0400

    test commit

--- Buttons.py
+++ Buttons.py
@@ -15,63 +15,73 @@ def actions0(row,col):
     value = counters[row][col]
     counters[row][col] = 0
     button_grid[row][col].config(text=counters[row][col])
-    if value > col and row == 0:  # if the value (number of stones) is grater then the index of the column and the row == 0
+    if value > col and row == 0:  # if the value (number of stones) is grater then the index of the colum and the row == 0
         wrap = value - col
-
-        # I added this so that the for loop below would work, and we could change value
-        # This solves you issue with the far left pit.
-        # note how I subtract one from value in both loops
-        runRow = value - wrap
-
-        for i in range(runRow):  # the for loop runs while i is <=value-wrap
+        for i in range(value - wrap):  # the for loop runs while i is <=value-wrap
             col -= 1
             counters[row][col] += 1
             button_grid[row][col].config(text=counters[row][col])
-            value-=1
         # col = 0
-
         for j in range(wrap):  # while j is <=wrap, the code below will run
             counters[1][col] += 1
             button_grid[1][col].config(text=counters[1][col])
             col += 1
-            value -= 1
-
+        col -= 1
+        row += 1
+        #print(row,col)
 
     # ok, so the code below is what was causing the issue
     # this code is supposed to handle the wrap back around, if the number is large enough
     # so you need the loop to start at the right most col
     # row needs to be starting at 0
     if value <= col and row == 0:  # if value <= the colum index and row == 0 the code below will run
+        row = 0
         for c in range(value):  # while a <= value, the code below will run
             col -= 1
             print("col",col)
             counters[row][col] += 1
             button_grid[row][col].config(text=counters[row][col])
+        #print(row,col)
 
 def actions1(row,col):
     global player
+    global counter1
     if row == 0:
         player = not player
         return
     value = counters[row][col]
     counters[row][col] = 0
     button_grid[row][col].config(text=counters[row][col])
-    if value > 6 - col and row == 1:  # if the value is larger then 6 - the index of the colum and the row == 1
+    if value > 6-col and row == 1:  # if the value (number of stones) is grater then the index of the column and the row == 0
         wrap = value - (6 - col)
-        for a in range(6 - col):  # while a <= 6-col the code below will run
+        row = 0
+        # I added this so that the for loop below would work, and we could change value
+        # This solves you issue with the far left pit.
+        # note how I subtract one from value in both loops
+        runRow = 6 - col
+
+        for i in range(runRow):  # the for loop runs while i is <=value-wrap
             col += 1
-            counters[row][col] += 1
-            button_grid[row][col].config(text=counters[row][col])
-        # col = 6
-        for b in range(wrap):  # while b is <=wrap, the code below will run
+            counters[1][col] += 1
+            button_grid[1][col].config(text=counters[1][col])
+        # col = 0
+
+        for j in range(wrap):  # while j is <=wrap, the code below will run
             counters[0][col] += 1
             button_grid[0][col].config(text=counters[0][col])
             col -= 1
+        col += 1
+        #print(row, col)
     if value <= 6 - col and row == 1:  # if the value is <= the index of the colum and the row = 0, the code below will run
+        row = 1
         for d in range(value):  # while a is <= value, the code below will run
             col += 1
             counters[row][col] += 1
             button_grid[row][col].config(text=counters[row][col])
+        #print(row,col)
+    if counters[row][col] == 1:
+        player = not player
+        counter1 += row[1][col]
 
 #this method determines what modifications are made to the board after a player clicks on a pit of their choice.
 #The modifications depends on what happens during the move, and what happens after. (i.e if a player lands in an empty pit)
@@ -104,7 +114,14 @@ for i in range(7):
     button_grid[0][i].config(fg="red")
     button_grid[1][i].config(fg="blue")
 
+root = tk.Tk()
+# Create a label to display text
+text_label = tk.Label(root, text=counter1, font=("Helvetica", 12))
+text_label.pack(pady=20)
 
+text_label = tk.Label(root, text=counter2, font=("Helvetica", 12))
+text_label.pack(pady=20)
 
-#used to run tkinter
+# Start the tkinter main loop
 root.mainloop()
+
